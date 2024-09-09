42383753879b80140361fbb60d08329f0dd29c06
4rtsy <pedrdev1704@gmail.com>
2024-09-09 18:54:05 -0300

    feat: Corrected the lines

--- double-triple-and-square-root.py
+++ double-triple-and-square-root.py
@@ -1,2 +1,2 @@
 n = int(input("write a number: "))
-print("the double of {} is {}, the triple is {} and the square root is {}".format(n, n*2, n*3, n**(1/2)))
+print("the double of {} is {}. \nThe triple is {}. \nAnd the square root is {}.".format(n, n*2, n*3, n**(1/2)))
