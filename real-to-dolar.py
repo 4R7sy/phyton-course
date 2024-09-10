f238bead09c52ddefa134efc99e96259790cb3d2
4rtsy <pedrdev1704@gmail.com>
2024-09-10 19:17:16 -0300

    feat: a converter of real-to-dolar

---
+++ real-to-dolar.py
@@ -0,0 +1,4 @@
+r = float(input("How many money do you have in your wallet?: "))
+##This is the value of the dollar on 9/10/2024##
+d = r / 5.66
+print("So with R${}, you could buy ${:.2f}".format(r, d))
