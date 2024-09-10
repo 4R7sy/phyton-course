f16bd18313b898203e9a4b890cd0abef8a0c696c
4rtsy <pedrdev1704@gmail.com>
2024-09-10 19:20:47 -0300

    feat: corrected some incomplete things

--- real-to-dolar.py
+++ real-to-dolar.py
@@ -1,4 +1,4 @@
-r = float(input("How many money do you have in your wallet?: "))
+r = float(input("How many money do you have in your wallet?: R$"))
 ##This is the value of the dollar on 9/10/2024##
 d = r / 5.66
-print("So with R${}, you could buy ${:.2f}".format(r, d))
+print("So with R${:.2f}, you could buy ${:.2f}".format(r, d))
