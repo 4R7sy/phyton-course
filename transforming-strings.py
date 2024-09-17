8de12d8c684561b20c4157b5a496c9a21a191f75
4rtsy <pedrdev1704@gmail.com>
2024-09-17 20:39:33 -0300

    feat: learning how to modify strings

---
+++ transforming-strings.py
@@ -0,0 +1,7 @@
+phrase = "I'm learning how to use this method"
+print(phrase[2:14])
+print(phrase.count("o"))
+print("-".join(phrase))
+print(phrase.split())
+print(len(phrase))
+print(phrase.replace("method", "transformation"))
