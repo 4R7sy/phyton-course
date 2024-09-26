d89e6463e58261f4562d2aa90bc2e56d54b90c3f
4rtsy <pedrdev1704@gmail.com>
2024-09-25 21:40:22 -0300

    feat: if and else fixed

--- even-and-odd.py
+++ even-and-odd.py
@@ -3,5 +3,5 @@ number = int(input("Write a random number: "))
 result = number % 2
 if result == 0:
     print(Fore.BLUE + "Your number is EVEN")
-if result == 1:
+else:
     print(Fore.GREEN + "Your number is ODD")
